26e81351f79b70a6cd298133e16871d51141557f
Fabio Correa <fabiosenracorrea@hotmail.com>
2024-08-30 10:35:48 -0300

    backoff adjusted

--- src/utils/backOff.ts
+++ src/utils/backOff.ts
@@ -1,11 +1,11 @@
-export function calculateExponentialBackOff(retries: number): number {
+function calculateExponentialBackOff(retries: number): number {
   const backOff = 2 ** retries * 10;
 
   return backOff;
 }
 
 // eslint-disable-next-line no-promise-executor-return
-export const wait = (ms: number): Promise<void> => new Promise((res) => setTimeout(res, ms));
+const wait = (ms: number): Promise<void> => new Promise((res) => setTimeout(res, ms));
 
 export async function waitExponentially(tries: number): Promise<void> {
   await wait(calculateExponentialBackOff(tries));
