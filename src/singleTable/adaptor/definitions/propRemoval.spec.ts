4fa49cfd66566608ed80273cf38cb81788f66a19
Fabio Correa <fabiosenracorrea@hotmail.com>
2024-09-01 16:23:31 -0300

    single table adaptor single table remover tests

---
+++ src/singleTable/adaptor/definitions/propRemoval.spec.ts
@@ -0,0 +1,283 @@
+import { cleanInternalProps, cleanInternalPropsFromList } from './propRemoval';
+
+describe('single table adaptor - prop cleaner helpers', () => {
+  describe('cleanInternalProps', () => {
+    it('should remove internal props by default', () => {
+      const result = cleanInternalProps(
+        {
+          _pk: 'some',
+          _sk: 'other',
+          _type: 'SOME_TYPE',
+          _ts: '123',
+          id: '1',
+          name: 'some',
+          other: 'prop',
+        },
+        {
+          table: 'db-table',
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          typeIndex: {
+            name: 'TypeIndexName',
+            partitionKey: '_type',
+            rangeKey: '_ts',
+          },
+        },
+      );
+
+      expect(result).toStrictEqual({
+        id: '1',
+        name: 'some',
+        other: 'prop',
+      });
+    });
+
+    it('should keep type property if defined', () => {
+      const result = cleanInternalProps(
+        {
+          _pk: 'some',
+          _sk: 'other',
+          _type: 'SOME_TYPE',
+          _ts: '123',
+          id: '1',
+          name: 'some',
+          other: 'prop',
+        },
+        {
+          table: 'db-table',
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          typeIndex: {
+            name: 'TypeIndexName',
+            partitionKey: '_type',
+            rangeKey: '_ts',
+          },
+          keepTypeProperty: true,
+        },
+      );
+
+      expect(result).toStrictEqual({
+        id: '1',
+        name: 'some',
+        other: 'prop',
+        _type: 'SOME_TYPE',
+      });
+    });
+
+    it('should not remove if explicity defined', () => {
+      const result = cleanInternalProps(
+        {
+          _pk: 'some',
+          _sk: 'other',
+          _type: 'SOME_TYPE',
+          _ts: '123',
+          id: '1',
+          name: 'some',
+          other: 'prop',
+        },
+        {
+          table: 'db-table',
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          typeIndex: {
+            name: 'TypeIndexName',
+            partitionKey: '_type',
+            rangeKey: '_ts',
+          },
+          autoRemoveTableProperties: false,
+        },
+      );
+
+      expect(result).toStrictEqual({
+        _pk: 'some',
+        _sk: 'other',
+        _type: 'SOME_TYPE',
+        _ts: '123',
+        id: '1',
+        name: 'some',
+        other: 'prop',
+      });
+    });
+
+    it('should use remover cb if present', () => {
+      const result = cleanInternalProps(
+        {
+          _pk: 'some',
+          _sk: 'other',
+          _type: 'SOME_TYPE',
+          _ts: '123',
+          id: '1',
+          name: 'some',
+          other: 'prop',
+        },
+        {
+          table: 'db-table',
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          typeIndex: {
+            name: 'TypeIndexName',
+            partitionKey: '_type',
+            rangeKey: '_ts',
+          },
+          propertyCleanup: ({ _sk, _ts, ...item }) => item,
+        },
+      );
+
+      expect(result).toStrictEqual({
+        _pk: 'some',
+        _type: 'SOME_TYPE',
+        id: '1',
+        name: 'some',
+        other: 'prop',
+      });
+    });
+  });
+
+  describe('cleanInternalPropsFromList', () => {
+    it('should remove internal props by default', () => {
+      const result = cleanInternalPropsFromList(
+        [
+          {
+            _pk: 'some',
+            _sk: 'other',
+            _type: 'SOME_TYPE',
+            _ts: '123',
+            id: '1',
+            name: 'some',
+            other: 'prop',
+          },
+        ],
+        {
+          table: 'db-table',
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          typeIndex: {
+            name: 'TypeIndexName',
+            partitionKey: '_type',
+            rangeKey: '_ts',
+          },
+        },
+      );
+
+      expect(result).toStrictEqual([
+        {
+          id: '1',
+          name: 'some',
+          other: 'prop',
+        },
+      ]);
+    });
+
+    it('should keep type property if defined', () => {
+      const result = cleanInternalPropsFromList(
+        [
+          {
+            _pk: 'some',
+            _sk: 'other',
+            _type: 'SOME_TYPE',
+            _ts: '123',
+            id: '1',
+            name: 'some',
+            other: 'prop',
+          },
+        ],
+        {
+          table: 'db-table',
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          typeIndex: {
+            name: 'TypeIndexName',
+            partitionKey: '_type',
+            rangeKey: '_ts',
+          },
+          keepTypeProperty: true,
+        },
+      );
+
+      expect(result).toStrictEqual([
+        {
+          id: '1',
+          name: 'some',
+          other: 'prop',
+          _type: 'SOME_TYPE',
+        },
+      ]);
+    });
+
+    it('should not remove if explicity defined', () => {
+      const result = cleanInternalPropsFromList(
+        [
+          {
+            _pk: 'some',
+            _sk: 'other',
+            _type: 'SOME_TYPE',
+            _ts: '123',
+            id: '1',
+            name: 'some',
+            other: 'prop',
+          },
+        ],
+        {
+          table: 'db-table',
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          typeIndex: {
+            name: 'TypeIndexName',
+            partitionKey: '_type',
+            rangeKey: '_ts',
+          },
+          autoRemoveTableProperties: false,
+        },
+      );
+
+      expect(result).toStrictEqual([
+        {
+          _pk: 'some',
+          _sk: 'other',
+          _type: 'SOME_TYPE',
+          _ts: '123',
+          id: '1',
+          name: 'some',
+          other: 'prop',
+        },
+      ]);
+    });
+
+    it('should use remover cb if present', () => {
+      const result = cleanInternalPropsFromList(
+        [
+          {
+            _pk: 'some',
+            _sk: 'other',
+            _type: 'SOME_TYPE',
+            _ts: '123',
+            id: '1',
+            name: 'some',
+            other: 'prop',
+          },
+        ],
+        {
+          table: 'db-table',
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          typeIndex: {
+            name: 'TypeIndexName',
+            partitionKey: '_type',
+            rangeKey: '_ts',
+          },
+          propertyCleanup: ({ _sk, _ts, ...item }) => item,
+        },
+      );
+
+      expect(result).toStrictEqual([
+        {
+          _pk: 'some',
+          _type: 'SOME_TYPE',
+          id: '1',
+          name: 'some',
+          other: 'prop',
+        },
+      ]);
+    });
+  });
+});
