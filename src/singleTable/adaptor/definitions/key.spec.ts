e26941c50dd641c805bc31fdaff135f799a085e5
Fabio Correa <fabiosenracorrea@hotmail.com>
2024-09-01 16:15:40 -0300

    single table key helpers tests

---
+++ src/singleTable/adaptor/definitions/key.spec.ts
@@ -0,0 +1,98 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { convertKey, getPrimaryKey } from './key';
+
+describe('single table key helpers', () => {
+  describe('convertKey', () => {
+    it('should handle non-array references', () => {
+      const values = [1, '1', null];
+
+      values.forEach((value) => {
+        expect(convertKey(value, {} as any)).toBe(`${value}`);
+      });
+    });
+
+    it('should handle array references with # as default joiner', () => {
+      const result = convertKey(['USER', 'some-id'], {} as any);
+
+      expect(result).toEqual('USER#some-id');
+    });
+
+    it('should handle array references with custom joiner', () => {
+      const result = convertKey(['USER', 'some-id'], {
+        keySeparator: '@',
+      } as any);
+
+      expect(result).toEqual('USER@some-id');
+    });
+
+    it('should handle single item array', () => {
+      const result = convertKey(['USER'], {} as any);
+
+      expect(result).toEqual('USER');
+    });
+  });
+
+  describe('getPrimaryKey', () => {
+    it('should properly reference the single table key', () => {
+      const key = getPrimaryKey(
+        {
+          partitionKey: 'USER',
+          rangeKey: 'some-id-99',
+        },
+        {
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          table: 'some_table',
+          typeIndex: {} as any,
+        },
+      );
+
+      expect(key).toStrictEqual({
+        _pk: 'USER',
+        _sk: 'some-id-99',
+      });
+    });
+
+    it('should handle array references with default joiner #', () => {
+      const key = getPrimaryKey(
+        {
+          partitionKey: 'USER',
+          rangeKey: ['0', 'some-id-99'],
+        },
+        {
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          table: 'some_table',
+          typeIndex: {} as any,
+        },
+      );
+
+      expect(key).toStrictEqual({
+        _pk: 'USER',
+        _sk: '0#some-id-99',
+      });
+    });
+
+    it('should handle array references with custom joiner', () => {
+      const key = getPrimaryKey(
+        {
+          partitionKey: 'USER',
+          rangeKey: ['0', 'some-id-99'],
+        },
+        {
+          partitionKey: '_pk',
+          rangeKey: '_sk',
+          table: 'some_table',
+          typeIndex: {} as any,
+          keySeparator: '@',
+        },
+      );
+
+      expect(key).toStrictEqual({
+        _pk: 'USER',
+        _sk: '0@some-id-99',
+      });
+    });
+  });
+});
