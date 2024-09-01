abf17b1993fc79cc5768db31ebdd366641973006
Fabio Correa <fabiosenracorrea@hotmail.com>
2024-09-01 16:06:58 -0300

    unused file removed

--- src/singleTable/adaptor/definitions/helpers.ts
+++
@@ -1,7 +0,0 @@
-import { SingleTableKeyReference } from './key';
-
-export type AsSingleTableParams<Params, RemoveKeys extends keyof Params> = Omit<
-  Params,
-  RemoveKeys
-> &
-  SingleTableKeyReference;
