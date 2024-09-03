490e3dfd5dbfbb558ef4c40e358c743fab8318a0
Fabio Correa <fabiosenracorrea@hotmail.com>
2024-09-03 20:23:44 -0300

    dynamodb v3: create tests

--- src/provider/utils/crud/create.spec.ts
+++ src/provider/utils/crud/create.spec.ts
@@ -12,7 +12,7 @@ const toJSON = (v: any): string => JSON.stringify(v, null, 2);
 
 describe('CreateItem actions', () => {
   describe('creation', () => {
-    it('should properly call dynamodb put operation', async () => {
+    it('v2: should properly call dynamodb put operation', async () => {
       const putMock = jest.fn().mockReturnValue({
         promise: jest.fn().mockResolvedValue({}),
       });
@@ -44,7 +44,41 @@ describe('CreateItem actions', () => {
       });
     });
 
-    it('should properly build conditions with its helpers', async () => {
+    it('v3: should properly call dynamodb put operation', async () => {
+      const putMock = jest.fn().mockReturnValue({});
+
+      const creator = new ItemCreator({
+        dynamoDB: {
+          target: 'v3',
+          instance: {
+            send: putMock,
+          } as any,
+        },
+      });
+
+      await creator.create({
+        table: 'table',
+        item: {
+          id: '23023',
+          hello: 'lalal',
+        },
+      });
+
+      expect(putMock).toHaveBeenCalledTimes(1);
+      expect(putMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          input: {
+            TableName: 'table',
+            Item: {
+              id: '23023',
+              hello: 'lalal',
+            },
+          },
+        }),
+      );
+    });
+
+    it('v2: should properly build conditions with its helpers', async () => {
       const putMock = jest.fn().mockReturnValue({
         promise: jest.fn().mockResolvedValue({}),
       });
@@ -101,7 +135,66 @@ describe('CreateItem actions', () => {
       });
     });
 
-    it('should log dynamodb params if option is passed', async () => {
+    it('v3: should properly build conditions with its helpers', async () => {
+      const putMock = jest.fn().mockReturnValue({});
+
+      const creator = new ItemCreator({
+        dynamoDB: {
+          target: 'v3',
+          instance: {
+            send: putMock,
+          } as any,
+        },
+      });
+
+      const conditions = [
+        {
+          operation: 'equal' as const,
+          value: 20,
+          property: 'some',
+        },
+        {
+          operation: 'in' as const,
+          values: [1, 2, 3],
+          property: 'list',
+        },
+      ];
+
+      await creator.create<any>({
+        table: 'table',
+        conditions,
+        item: {
+          id: '23023',
+          hello: 'lalal',
+        },
+      });
+
+      expect(putMock).toHaveBeenCalledTimes(1);
+      expect(putMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          input: {
+            TableName: 'table',
+
+            Item: {
+              id: '23023',
+              hello: 'lalal',
+            },
+
+            // These helpers have their own testing
+            // and are here because are the only source of Condition transformations
+            // that should be used across the provider
+
+            ConditionExpression: buildConditionExpression(conditions),
+
+            ExpressionAttributeNames: getConditionExpressionNames(conditions),
+
+            ExpressionAttributeValues: getConditionExpressionValues(conditions),
+          },
+        }),
+      );
+    });
+
+    it('v2: should log dynamodb params if option is passed', async () => {
       const consoleLogMock = jest.spyOn(console, 'log').mockImplementation(() => {});
 
       const creator = new ItemCreator({
@@ -113,6 +206,41 @@ describe('CreateItem actions', () => {
             put: () => ({
               promise: () => {},
             }),
+          } as any,
+        },
+      });
+
+      await creator.create({
+        table: 'table',
+        item: {
+          id: '23023',
+          hello: 'lalal',
+        },
+      });
+
+      expect(consoleLogMock).toHaveBeenCalledWith(
+        toJSON({
+          TableName: 'table',
+          Item: {
+            id: '23023',
+            hello: 'lalal',
+          },
+        }),
+      );
+
+      consoleLogMock.mockRestore();
+    });
+
+    it('v3: should log dynamodb params if option is passed', async () => {
+      const consoleLogMock = jest.spyOn(console, 'log').mockImplementation(() => {});
+
+      const creator = new ItemCreator({
+        logCallParams: true,
+
+        dynamoDB: {
+          target: 'v3',
+          instance: {
+            send: () => {},
           } as any,
         },
       });
