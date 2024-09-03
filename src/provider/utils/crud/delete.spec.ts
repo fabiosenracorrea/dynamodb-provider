25edfb622c136b2d348f5f9992ac12f70f34ec4f
Fabio Correa <fabiosenracorrea@hotmail.com>
2024-09-03 20:21:48 -0300

    dynamo db v3 delete tests

--- src/provider/utils/crud/delete.spec.ts
+++ src/provider/utils/crud/delete.spec.ts
@@ -12,7 +12,7 @@ const toJSON = (v: any): string => JSON.stringify(v, null, 2);
 
 describe('DeleteItem actions', () => {
   describe('deletion', () => {
-    it('should properly call dynamodb delete operation', async () => {
+    it('v2: should properly call dynamodb delete operation', async () => {
       const deleteMock = jest.fn().mockReturnValue({
         promise: jest.fn().mockResolvedValue({}),
       });
@@ -44,7 +44,41 @@ describe('DeleteItem actions', () => {
       });
     });
 
-    it('should properly build conditions with its helpers', async () => {
+    it('v3: should properly call dynamodb delete operation', async () => {
+      const deleteMock = jest.fn().mockReturnValue({});
+
+      const remover = new ItemRemover({
+        dynamoDB: {
+          target: 'v3',
+          instance: {
+            send: deleteMock,
+          } as any,
+        },
+      });
+
+      await remover.delete({
+        table: 'table',
+        key: {
+          id: '23023',
+          hello: 'lalal',
+        },
+      });
+
+      expect(deleteMock).toHaveBeenCalledTimes(1);
+      expect(deleteMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          input: {
+            TableName: 'table',
+            Key: {
+              id: '23023',
+              hello: 'lalal',
+            },
+          },
+        }),
+      );
+    });
+
+    it('v2: should properly build conditions with its helpers', async () => {
       const deleteMock = jest.fn().mockReturnValue({
         promise: jest.fn().mockResolvedValue({}),
       });
@@ -101,7 +135,66 @@ describe('DeleteItem actions', () => {
       });
     });
 
-    it('should log dynamodb params if option is passed', async () => {
+    it('v3: should properly build conditions with its helpers', async () => {
+      const deleteMock = jest.fn().mockReturnValue({});
+
+      const remover = new ItemRemover({
+        dynamoDB: {
+          target: 'v3',
+          instance: {
+            send: deleteMock,
+          } as any,
+        },
+      });
+
+      const conditions = [
+        {
+          operation: 'equal' as const,
+          value: 20,
+          property: 'some',
+        },
+        {
+          operation: 'in' as const,
+          values: [1, 2, 3],
+          property: 'list',
+        },
+      ];
+
+      await remover.delete<any>({
+        table: 'table',
+        conditions,
+        key: {
+          id: '23023',
+          hello: 'lalal',
+        },
+      });
+
+      expect(deleteMock).toHaveBeenCalledTimes(1);
+      expect(deleteMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          input: {
+            TableName: 'table',
+
+            Key: {
+              id: '23023',
+              hello: 'lalal',
+            },
+
+            // These helpers have their own testing
+            // and are here because are the only source of Condition transformations
+            // that should be used across the provider
+
+            ConditionExpression: buildConditionExpression(conditions),
+
+            ExpressionAttributeNames: getConditionExpressionNames(conditions),
+
+            ExpressionAttributeValues: getConditionExpressionValues(conditions),
+          },
+        }),
+      );
+    });
+
+    it('v2: should log dynamodb params if option is passed', async () => {
       const consoleLogMock = jest.spyOn(console, 'log').mockImplementation(() => {});
 
       const remover = new ItemRemover({
@@ -113,6 +206,41 @@ describe('DeleteItem actions', () => {
             delete: () => ({
               promise: () => {},
             }),
+          } as any,
+        },
+      });
+
+      await remover.delete({
+        table: 'table',
+        key: {
+          id: '23023',
+          hello: 'lalal',
+        },
+      });
+
+      expect(consoleLogMock).toHaveBeenCalledWith(
+        toJSON({
+          TableName: 'table',
+          Key: {
+            id: '23023',
+            hello: 'lalal',
+          },
+        }),
+      );
+
+      consoleLogMock.mockRestore();
+    });
+
+    it('v3: should log dynamodb params if option is passed', async () => {
+      const consoleLogMock = jest.spyOn(console, 'log').mockImplementation(() => {});
+
+      const remover = new ItemRemover({
+        logCallParams: true,
+
+        dynamoDB: {
+          target: 'v3',
+          instance: {
+            send: () => {},
           } as any,
         },
       });
