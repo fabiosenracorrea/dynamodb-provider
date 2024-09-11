edd20e586b29f217c7598a7edf0e926573e54fb3
Fabio Correa <fabiosenracorrea@hotmail.com>
2024-09-10 22:31:49 -0300

    key resolvers tests

---
+++ src/singleTable/model/definitions/key/resolveKey.spec.ts
@@ -0,0 +1,39 @@
+import { resolveKeys } from './resolveKey';
+
+describe('single table model - key resolver', () => {
+  const mockGetPartitionKey = jest.fn((params) => `partition-${params.id}`);
+  const mockGetRangeKey = jest.fn((params) => `range-${params.id}`);
+
+  const params = {
+    getPartitionKey: mockGetPartitionKey,
+    getRangeKey: mockGetRangeKey,
+  };
+
+  it('should return the getPartitionKey function correctly', () => {
+    const result = resolveKeys(params);
+
+    expect(result.getPartitionKey).toBe(mockGetPartitionKey);
+  });
+
+  it('should return the getRangeKey function correctly', () => {
+    const result = resolveKeys(params);
+
+    expect(result.getRangeKey).toBe(mockGetRangeKey);
+  });
+
+  it('should return a getKey function that correctly resolves the keys', () => {
+    const result = resolveKeys(params);
+
+    const keyParams = { id: '123' };
+    const expectedPartitionKey = `partition-${keyParams.id}`;
+    const expectedRangeKey = `range-${keyParams.id}`;
+
+    const keys = result.getKey(keyParams);
+
+    expect(keys.partitionKey).toBe(expectedPartitionKey);
+    expect(keys.rangeKey).toBe(expectedRangeKey);
+
+    expect(mockGetPartitionKey).toHaveBeenCalledWith(keyParams);
+    expect(mockGetRangeKey).toHaveBeenCalledWith(keyParams);
+  });
+});
