819eb4e8e7c77fbbca96cc0d6692a54e9e5538c3
Fabio Correa <fabiosenracorrea@hotmail.com>
2024-10-03 17:47:28 -0300

    collection get doc v

--- src/singleTable/fromCollection/definitions/constructor.ts
+++ src/singleTable/fromCollection/definitions/constructor.ts
@@ -3,6 +3,13 @@ import { ExtendableCollection } from 'singleTable/model';
 import { GetCollectionParams, GetCollectionResult } from './get';
 
 export type FromCollection<Registered extends ExtendableCollection> = {
+  /**
+   * Retrieve your collection
+   *
+   * IMPORTANT: Currently, this only supports full retrievals, meaning
+   * the data fetch will continue as long as the collection query keeps
+   * yielding items
+   */
   get(...params: GetCollectionParams<Registered>): Promise<GetCollectionResult<Registered>>;
 };
 
