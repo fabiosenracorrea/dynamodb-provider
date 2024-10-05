7a67fd3e0fca10aa3d88f86aa479bd622d9be2b5
Fabio Correa <fabiosenracorrea@hotmail.com>
2024-10-05 17:43:45 -0300

    jest ignore js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '**.js'],
   moduleDirectories: ['node_modules', './src'],
 
   // disable type checks on tests
