4ca1d94ed5a363a2c275b16611973fb4f8eab534
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-12-10 10:51:38 -0300

    ExtractTableConfig docs

--- src/singleTable/types.ts
+++ src/singleTable/types.ts
@@ -1,3 +1,13 @@
 import { SingleTable } from './implementation';
 
+/**
+ * Extracts the SingleTable configuration type
+ *
+ * @example
+ * ```ts
+ * const table = new SingleTable({...config})
+ *
+ * type TableConfig = ExtractTableConfig<typeof table>
+ * ```
+ */
 export type ExtractTableConfig<T> = T extends SingleTable<infer X> ? X : never;
