c32116188314c4ebfabf089d5ece183efb8b9483
Fabio Correa <fabiosenracorrea@hotmail.com>
2026-01-04 11:43:10 -0300

    listing shown only after click

--- packages/playground/src/client/components/operations/ListForm.tsx
+++ packages/playground/src/client/components/operations/ListForm.tsx
@@ -86,14 +86,16 @@ export function ListForm({ target, name }: ListFormProps) {
         </Button>
       </div>
 
-      <div className="pt-4 border-t">
-        <h4 className="text-sm font-medium mb-2">Result</h4>
-        <ListResultView
-          data={result}
-          error={error ?? undefined}
-          entityType={target === 'entity' ? name : undefined}
-        />
-      </div>
+      {!!mutation.data && (
+        <div className="pt-4 border-t">
+          <h4 className="text-sm font-medium mb-2">Result</h4>
+          <ListResultView
+            data={result}
+            error={error ?? undefined}
+            entityType={target === 'entity' ? name : undefined}
+          />
+        </div>
+      )}
     </div>
   );
 }
