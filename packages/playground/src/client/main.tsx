c70ae4c5c106b522e2aa9fc151172f20f1c8303d
Fabio Correa <fabiosenracorrea@hotmail.com>
2026-08-11 13:53:56 -0300

    lint

--- packages/playground/src/client/main.tsx
+++ packages/playground/src/client/main.tsx
@@ -24,5 +24,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </MetadataProvider>
       </QueryClientProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
